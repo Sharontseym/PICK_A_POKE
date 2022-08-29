71c06bc2c3c5b9618d68f280fefb84931824a509
Yanming Tse <tseyan113@gmail.com>
2022-08-29 16:22:07 +0100

    created pokemon model

---
+++ db/migrate/20220829151714_create_pokemons.rb
@@ -0,0 +1,12 @@
+class CreatePokemons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pokemons do |t|
+      t.string   :name
+      t.string   :type
+      t.string   :move
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
