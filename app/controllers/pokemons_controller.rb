6935e61ee18cbb1e4d617f7234befa2c0fee607b
rory jackson <roryjamesjackson@hotmail.com>
2022-09-01 15:55:00 +0100

    add dotenv-rails

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -14,4 +14,23 @@ class PokemonsController < ApplicationController
     @pokemon = Pokemon.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    if @pokemon.save
+      redirect_to_pokemon_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :element, :move, :user_id)
+  end
 end
